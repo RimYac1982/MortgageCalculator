4c570b58c67664c86d0660566fee5d416cfa4e2c
Rimon <rimon_yacoub@yahoo.ca>
2024-10-17 16:11:03 -0700

    added HomeConroller class

--- src/application/HomeController.java
+++ src/application/HomeController.java
@@ -1,77 +1,84 @@
 package application;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Tooltip;
+
+
 import java.text.NumberFormat;
 
 public class HomeController {
+
     private NumberFormat Currency = NumberFormat.getCurrencyInstance();
-    ObservableList<String> LoanDurationList = FXCollections.observableArrayList("Custom", "Selected");
 
     @FXML
     private Slider LoanDurationSelected;
 
     @FXML
-    private TextField LoanDurationCustom;
+    private TextField monthlyIncomeField;
 
     @FXML
-    private ChoiceBox<String> LoanDurationSelector;
+    private TextField monthlyExpensesField;
 
     @FXML
-    private TextField DownPaymentPrice;
+    private TextField InterestRateField;
 
     @FXML
-    private TextField InterestRate;
+    private TextField DownPaymentField;
 
     @FXML
-    private TextField PurchasePrice;
-
+    private TextField PurchasePriceField;
+    
+    @FXML
+    private TextField InsuranceRateField;
+    
     @FXML
     private TextField TotalPayment;
 
     @FXML
-    private void initialize() {
-        LoanDurationSelector.setItems(LoanDurationList);
-        LoanDurationSelector.setTooltip(new Tooltip("Custom or Selected?"));
-    }
+    private Button Exit;
+
+    @FXML
+    private Button Clear;
+    
+    @FXML
+    private Button Calculate;
+
+    @FXML
+    private Label RecommendedHousePrice;  
 
     @FXML
     void CalculateButtonPressed(ActionEvent event) {
-        double PurchasePriceAmount = Double.parseDouble(PurchasePrice.getText());
-        double DownPaymentPriceAmount = Double.parseDouble(DownPaymentPrice.getText());
-        double InterestRateAmount = Double.parseDouble(InterestRate.getText());
-        double Loan;
-        if (LoanDurationSelector.getValue().equals("Custom")) {
-            Loan = Double.parseDouble(LoanDurationCustom.getText());
-        } else {
-            Loan = LoanDurationSelected.getValue();
-        }
-        double Principle = PurchasePriceAmount - DownPaymentPriceAmount;
-        InterestRateAmount = InterestRateAmount / 100;
-        double InterestRateMonth = InterestRateAmount / 12;
-        double LoanMonth = Loan * 12;
-        double top = (InterestRateMonth * Math.pow(1 + InterestRateMonth, LoanMonth));
-        double bottom = (Math.pow(1 + InterestRateMonth, LoanMonth) - 1);
-        double Total = Principle * (top / bottom);
-        TotalPayment.setText(Currency.format(Total));
+                double PurchasePriceAmount = Double.parseDouble(PurchasePriceField.getText());
+                double DownPaymentPriceAmount = Double.parseDouble(DownPaymentField.getText());
+                double InterestRateAmount = Double.parseDouble(InterestRateField.getText());
+                double Loan = LoanDurationSelected.getValue();         
+                double Principle = PurchasePriceAmount - DownPaymentPriceAmount;
+                InterestRateAmount = InterestRateAmount / 100;
+                double InterestRateMonth = InterestRateAmount / 12;
+                double LoanMonth = Loan * 12;
+                double top = (InterestRateMonth * Math.pow(1 + InterestRateMonth, LoanMonth));
+                double bottom = (Math.pow(1 + InterestRateMonth, LoanMonth) - 1);
+                double Total = Principle * (top / bottom);
+                TotalPayment.setText(Currency.format(Total));           
     }
+
+
 
     // Method to clear all input fields
     @FXML
     void ClearForm(ActionEvent event) {
-        PurchasePrice.clear();
-        DownPaymentPrice.clear();
-        InterestRate.clear();
-        LoanDurationCustom.clear();
-        TotalPayment.clear();
-        LoanDurationSelector.setValue(null); // Reset ChoiceBox
-        LoanDurationSelected.setValue(20); // Reset Slider to default value (20)
+        PurchasePriceField.clear();
+        DownPaymentField.clear();
+        InterestRateField.clear();
+        monthlyIncomeField.clear();
+        monthlyExpensesField.clear();
+        InsuranceRateField.clear();
+        TotalPayment.setText("");           
+        LoanDurationSelected.setValue(20);  
     }
 
     @FXML
@@ -79,19 +86,14 @@ public class HomeController {
         System.exit(0);
     }
 
-    // New method to calculate the recommended house price
     @FXML
     void CalculateRecommendedPrice(ActionEvent event) {
-        // Implement your logic for calculating the recommended house price here
-        // For example, it might be based on income, expenses, and loan term
         System.out.println("Calculating recommended house price...");
     }
 
-    // New method to view the amortization schedule
+    // New method to view the amortization schedule (implement logic)
     @FXML
     void ViewAmortizationSchedule(ActionEvent event) {
-        // Implement your logic for showing the amortization schedule here
-        // For example, open a new window or display the amortization breakdown
         System.out.println("Viewing amortization schedule...");
     }
 }
