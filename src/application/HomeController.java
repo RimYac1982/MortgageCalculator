f8fe82b5ecd3fd678eaca517e1b9ec69d2bf1c6f
Rimon Yacoub <rimon.yacoub@sjsu.edu>
2024-10-22 12:18:12 -0700

    HomeAffortability Calculator working now

--- src/application/HomeController.java
+++ src/application/HomeController.java
@@ -3,6 +3,7 @@ package application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -177,12 +178,12 @@ public class HomeController {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/HomeAffordabilityCalculator.fxml"));
             Parent root = loader.load();
-            Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root));
             stage.setTitle("Home Affordability Calculator");
             stage.show();
         } catch (IOException e) {
-            e.printStackTrace(); 
+            e.printStackTrace();           
         }
     }
 
